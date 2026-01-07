b44c02487588fa12e8b89d440f7f984df5c6e639
KING <matadeenyadav202@gmail.com>
2026-01-07 19:43:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const variable = 100 
+var aman = 12
+let amn = 13
+console.log("hey");
+console.info("heyhey");
+console.warn("XYZ");
+console.error("404");
+console.table({name : "Aman"});
+// let nem = prompt("Name?")
+// console.log(nem);
+alert("Something-Something");
